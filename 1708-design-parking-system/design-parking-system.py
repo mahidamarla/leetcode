de1f71293c78ae72fad5cf8609c6c3add6ad786b
mahidamarla <mdamarla2@gmail.com>
2026-08-01 11:51:19 +0530

    Time: 5 ms (93.16%) | Memory: 19.7 MB (90.08%) - LeetSync

---
+++ 1708-design-parking-system/design-parking-system.py
@@ -0,0 +1,25 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big=big
+        self.medium=medium
+        self.small=small
+    def addCar(self, carType: int) -> bool:
+        if self.big>0 and carType==1:
+            self.big-=1
+            return True
+        elif self.medium>0 and carType==2:
+            self.medium-=1
+            return True
+        elif self.small>0 and carType==3:
+            self.small-=1
+            return True
+        else:
+            return False
+
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
